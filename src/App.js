4d7b33dd45a32f98885d42d9ea5f43231b1e07eb
Earthpasserby <iwuno68@gmail.com>
2024-06-13 12:03:56 +0100

    more

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
+import 
 
 function App() {
-  const [products, setPoduct] = useState(
-    [{category:'glass', Image:'', price:'$400'},
-      {category:'glass', Image:'', price:'$400'}
-    ]
-  );
+  const [products, setPoduct] = useState([
+    {
+      category: "glass",
+      Image:
+        "https://img.freepik.com/premium-photo/portrait-young-woman-using-mobile-phone_1048944-30344856.jpg",
+      price: "$400",
+    },
+    {
+      category: ["design", "red"],
+      Image:
+        "https://img.freepik.com/premium-photo/business-people-working-office_1048944-30369177.jpg?w=900",
+      price: "$400",
+    },
+    {
+      category: ["design", "red"],
+      Image:
+        "https://img.freepik.com/premium-photo/business-people-working-office_1048944-30369177.jpg?w=900",
+      price: "$400",
+    },
+    {
+      category: ["design", "red"],
+      Image:
+        "https://img.freepik.com/premium-photo/business-people-working-office_1048944-30369177.jpg?w=900",
+      price: "$400",
+    },
+    {
+      category: ["design", "red"],
+      Image:
+        "https://img.freepik.com/premium-photo/business-people-working-office_1048944-30369177.jpg?w=900",
+      price: "$400",
+    },
+    {
+      category: ["design", "red"],
+      Image:
+        "https://img.freepik.com/premium-photo/business-people-working-office_1048944-30369177.jpg?w=900",
+      price: "$400",
+    },
+  ]);
   return (
-    <div className="App">
+    <div className="App ">
       <h1>what do you think</h1>
+      {products.map((product) => {
+        console.log(product);
+      <Product name/>  
+      })}
     </div>
   );
 }
